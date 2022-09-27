19c0281d1fd2edd30299d1ca22c6fb5a0f2068ab
AYESIGYE DERRICK <ayesigyederrick@gmail.com>
2022-09-27 09:51:15 +0300

    Added a new class mybag

--- classes.py
+++ classes.py
@@ -15,3 +15,20 @@ print(my_list.length)
 my_list.appendmethod(6)
 print(my_list.data)
 print(my_list.length)
+
+class Bag:
+    def __init__(self):
+        self.data = []
+
+    def add(self, x):
+        self.data.append(x)
+
+    def addtwice(self, x): #appends item twice
+        self.add(x)
+        self.add(x)
+
+bag1 = Bag() #Instance 1 of the bag class
+bag1.add([1,2,3])
+print(bag1.data)
+bag1.addtwice([2,3,4])
+print(bag1.data)
