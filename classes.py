79e9fa8a15157dd6f7fd70c9e94d40172795b6c4
AYESIGYE DERRICK <ayesigyederrick@gmail.com>
2022-09-27 09:45:00 +0300

    Created First instance of MyList

--- classes.py
+++ classes.py
@@ -8,3 +8,10 @@ class MyList:  #List impelementation in python
     def appendmethod(self,new_item):
         self.data = self.data +[new_item]
         self.length +=1
+
+my_list = MyList([1,2,3,4,5])
+print(my_list.data)
+print(my_list.length)
+my_list.appendmethod(6)
+print(my_list.data)
+print(my_list.length)
